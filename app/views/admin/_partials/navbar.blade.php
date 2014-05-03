84e0634c5623980596e357430fd42670cbcee0cf
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2014-05-03 12:32:18 +0300

    Update navigation for testing

--- app/views/admin/_partials/navbar.blade.php
+++ app/views/admin/_partials/navbar.blade.php
@@ -11,6 +11,7 @@
 	<!-- /.navbar-header -->
 
 	<ul class="nav navbar-top-links navbar-right">
+	<!--
 		<li class="dropdown">
 			<a class="dropdown-toggle" data-toggle="dropdown" href="#">
 				<i class="fa fa-envelope fa-fw"></i>  <i class="fa fa-caret-down"></i>
@@ -59,9 +60,9 @@
 					</a>
 				</li>
 			</ul>
-			<!-- /.dropdown-messages -->
+			
 		</li>
-		<!-- /.dropdown -->
+		
 		<li class="dropdown">
 			<a class="dropdown-toggle" data-toggle="dropdown" href="#">
 				<i class="fa fa-tasks fa-fw"></i>  <i class="fa fa-caret-down"></i>
@@ -138,9 +139,9 @@
 					</a>
 				</li>
 			</ul>
-			<!-- /.dropdown-tasks -->
+			
 		</li>
-		<!-- /.dropdown -->
+		
 		<li class="dropdown">
 			<a class="dropdown-toggle" data-toggle="dropdown" href="#">
 				<i class="fa fa-bell fa-fw"></i>  <i class="fa fa-caret-down"></i>
@@ -198,19 +199,21 @@
 					</a>
 				</li>
 			</ul>
-			<!-- /.dropdown-alerts -->
+			
 		</li>
-		<!-- /.dropdown -->
+		-->
 		<li class="dropdown">
 			<a class="dropdown-toggle" data-toggle="dropdown" href="#">
 				<i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
 			</a>
 			<ul class="dropdown-menu dropdown-user">
+			<!--
 				<li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
 				</li>
 				<li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
 				</li>
 				<li class="divider"></li>
+			-->
 				<li><a href="{{ URL::route('admin.logout') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
 				</li>
 			</ul>
