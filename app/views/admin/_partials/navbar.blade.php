e0e10e31c1b0d7364b9b8f8a5bb8fcc4afa8a22f
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2014-05-08 16:32:01 +0300

    Fixed navbar link

--- app/views/admin/_partials/navbar.blade.php
+++ app/views/admin/_partials/navbar.blade.php
@@ -6,7 +6,7 @@
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
 		</button>
-		<a class="navbar-brand" href="index.html">Demo blog - Control Panel</a>
+		<a class="navbar-brand" href="{{ URL::route('admin.dashboard') }}">Demo blog - Control Panel</a>
 	</div>
 	<!-- /.navbar-header -->
 
