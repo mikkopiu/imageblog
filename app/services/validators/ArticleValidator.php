dcb10b66e8b9fed5e0ca5c47489d5243c9dbb27d
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2014-05-08 19:08:41 +0300

    Added validation rules to ArticleValidator

--- app/services/validators/ArticleValidator.php
+++ app/services/validators/ArticleValidator.php
@@ -5,11 +5,12 @@ class ArticleValidator extends Validator {
 	public static $messages = [
 			'title.required'	=> 'Please type in a title.',
 			'title.regex'		=> 'Your title contains illegal characters.',
-			'category'			=> 'Please select a category, so that people can find your post.'
+			'category.required'			=> 'Please select a category, so that people can find your post.',
+			'category.regex'	=> 'Your category name may only contain letters, numbers, spaces, dashes (-) and underscores (_).'
 	];
 
 	public static $rules = [
 		'title'		=> 'required|regex:/^[a-z0-9 .\-\_\(\)\&\%\"\'\?\!\#\/\=\´\`\£\$\€\,\:\+]+$/i',
-		'category'		=> 'required'
+		'category'		=> 'required|regex:/^[a-z0-9 .\-\_]+$/i'
 	];
 }
