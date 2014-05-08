5fdf5a4b798eee39147e9186a6c84fb0e85ab388
Mikko Piuhola <mikko.piuhola@metropolia.fi>
2014-05-08 22:57:42 +0300

    Updated comment validation rules

--- app/services/validators/CommentValidator.php
+++ app/services/validators/CommentValidator.php
@@ -5,11 +5,12 @@ class CommentValidator extends Validator {
 	public static $messages = [
 		'commenter.required'	=> 'Please type in your name.',
 		'commenter.alpha_num'	=> 'Your name may only contain letters and numbers',
-		'body'					=> 'Please type in your comment.'
+		'body.required'			=> 'Please type in your comment.',
+		'body.regex'			=> 'Your comment contains illegal characters.'
 	];
 
 	public static $rules = [
 		'commenter'		=> 'required|alpha_num',
-		'body'			=> 'required'
+		'body'			=> 'required|regex:/^[a-z0-9 .\-\_\(\)\&\%\"\'\?\!\#\/\=\´\`\£\$\€\,\:\+]+$/i'
 	];
 }
